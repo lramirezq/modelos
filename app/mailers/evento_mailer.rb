90158733a1066473c7fdadaec827a9db0603e916
Luis Ramirez Queupul <ramirezqueupul@gmail.com>
2013-02-19 06:52:22 -0300

    Revert "	modified:   app/mailers/evento_mailer.rb"
    This reverts commit 2f0ae6e64c417a2ea633eeff84a1a99ba65930b9.
    
    Conflicts:
    	app/mailers/evento_mailer.rb

--- app/mailers/evento_mailer.rb
+++ app/mailers/evento_mailer.rb
@@ -1,4 +1,9 @@
 class EventoMailer < ActionMailer::Base
   default :from => "ramirezqueupul@gmail.com"
   
+<<<<<<< HEAD
+=======
+  
+  
+>>>>>>> parent of 2f0ae6e... 	modified:   app/mailers/evento_mailer.rb
 end
