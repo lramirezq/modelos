fcd291c85d6e2496f6d39ae27350c6c936c43a21
Luis Ramirez Queupul <ramirezqueupul@gmail.com>
2013-02-19 06:52:39 -0300

    Revert "Alla va"
    This reverts commit 4c77f492bdfb84e0959244ec8505703926f90b1c.
    
    Conflicts:
    	app/mailers/evento_mailer.rb

--- app/mailers/evento_mailer.rb
+++ app/mailers/evento_mailer.rb
@@ -1,9 +1,12 @@
 class EventoMailer < ActionMailer::Base
   default :from => "ramirezqueupul@gmail.com"
+<<<<<<< HEAD
   
 <<<<<<< HEAD
 =======
   
   
 >>>>>>> parent of 2f0ae6e... 	modified:   app/mailers/evento_mailer.rb
+=======
+>>>>>>> parent of 4c77f49... Alla va
 end
