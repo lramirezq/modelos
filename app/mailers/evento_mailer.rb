c3488ed87e7d39d070b5aae6c82205f0349f276a
Luis Ramirez Queupul <ramirezqueupul@gmail.com>
2013-03-10 22:45:35 -0400

    Listo el informe a contabilidad

--- app/mailers/evento_mailer.rb
+++ app/mailers/evento_mailer.rb
@@ -5,11 +5,12 @@ class EventoMailer < ActionMailer::Base
  
   # Metodo de envio para informes 
   def enviar_informes(competencia, root_url)
+   
     @root_url = root_url.to_s.gsub(/\/+$/, '')
     @competencia = competencia
     @destinos = Mantenedor.mail_evento.collect {|d| d.valor}.join(', ')
     puts "Esto llego aca !" + competencia.tipocompetencia.to_s
     #Recibo destinos desde el mantenedor    
-    mail(:to =>  @destinos, :subject => "New Models | Envio Detalle de Competencias")  
+    mail(:to =>  @destinos, :subject => "New Models | Envio Detalle de Competencias -  #{competencia.id.to_s}")  
   end
 end
